0f1e21f9d294e8fd420e6059321799618931b71a
RyadDevweb <ibryadtedjani@gmail.com>
2026-06-01 14:14:43 +0200

    modify size

--- carnetDeVoyage/ContentView.swift
+++ carnetDeVoyage/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Text("Carnet de voyage !")
+                .font( .system(size: 40))
             Image(systemName: "person")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
