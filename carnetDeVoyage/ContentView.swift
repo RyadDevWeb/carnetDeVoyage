1d5af9a94d762b8ec2e7fdc1cf8a618649fd7d2d
RyadDevweb <ibryadtedjani@gmail.com>
2026-06-01 14:10:06 +0200

    modify title

---
+++ carnetDeVoyage/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  carnetDeVoyage
+//
+//  Created by Apprenant122 on 01/06/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Carnet de voyage !")
+            Image(systemName: "person")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ carnetDeVoyage/carnetDeVoyageApp.swift
@@ -0,0 +1,17 @@
+//
+//  carnetDeVoyageApp.swift
+//  carnetDeVoyage
+//
+//  Created by Apprenant122 on 01/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct carnetDeVoyageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
